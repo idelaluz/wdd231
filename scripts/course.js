3dae39e91ae485151898d9935622a82240d5eaea
idelaluz <israel.delaluz@gmail.com>
2025-05-21 23:41:14 +0200

    Commit 21.05.2025 23h41

---
+++ scripts/course.js
@@ -0,0 +1,22 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const topButtons = ["All", "CSE", "WDD"];
+    const courseButtons = ["CSE 110", "WDD 130", "CSE 111", "CSE 210", "WDD 131", "WDD 231"];
+
+    const topContainer = document.querySelector(".top-buttons");
+    const courseContainer = document.querySelector(".course-buttons");
+
+    topButtons.forEach(label => {
+      const btn = document.createElement("button");
+      btn.textContent = label;
+      topContainer.appendChild(btn);
+    });
+
+    courseButtons.forEach(label => {
+      const btn = document.createElement("button");
+      btn.textContent = label;
+      courseContainer.appendChild(btn);
+    });
+  });
+</script>
+
