6c4aad94517f7ce011cf1140f679696dfbfeb1f2
idelaluz <israel.delaluz@gmail.com>
2025-05-21 23:44:22 +0200

    Commit 21.05.2025 23h44

--- scripts/course.js
+++ scripts/course.js
@@ -1,22 +1,20 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    const topButtons = ["All", "CSE", "WDD"];
-    const courseButtons = ["CSE 110", "WDD 130", "CSE 111", "CSE 210", "WDD 131", "WDD 231"];
+document.addEventListener("DOMContentLoaded", function() {
+  const topButtons = ["All", "CSE", "WDD"];
+  const courseButtons = ["CSE 110", "WDD 130", "CSE 111", "CSE 210", "WDD 131", "WDD 231"];
 
-    const topContainer = document.querySelector(".top-buttons");
-    const courseContainer = document.querySelector(".course-buttons");
+  const topContainer = document.querySelector(".top-buttons");
+  const courseContainer = document.querySelector(".course-buttons");
 
-    topButtons.forEach(label => {
-      const btn = document.createElement("button");
-      btn.textContent = label;
-      topContainer.appendChild(btn);
-    });
-
-    courseButtons.forEach(label => {
-      const btn = document.createElement("button");
-      btn.textContent = label;
-      courseContainer.appendChild(btn);
-    });
+  topButtons.forEach(label => {
+    const btn = document.createElement("button");
+    btn.textContent = label;
+    topContainer.appendChild(btn);
   });
-</script>
+
+  courseButtons.forEach(label => {
+    const btn = document.createElement("button");
+    btn.textContent = label;
+    courseContainer.appendChild(btn);
+  });
+});
 
