21e20183e22ccd4cabcaefd4deeb2a67df7baa4f
idelaluz <israel.delaluz@gmail.com>
2025-05-23 14:10:25 +0200

    Commit 23.05.2025 14h10

--- scripts/course.js
+++ scripts/course.js
@@ -28,3 +28,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
